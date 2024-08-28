aa0b18517bb5f15f2af2e2ca24e8304c0706256c
josMuiruri <josemuiruri51@gmail.com>
2024-08-28 18:14:22 +0300

    pep8 validation

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 '''a function that returns a list of lists of integers
 representing the Pascal's triangle'''
+
+
 def pascal_triangle(n: int) -> list:
     ''' assigning an empty list '''
     triangle = []
-    
     ''' less than/equal to 0 return an empty list '''
     if n <= 0:
         return triangle
     # looping i = rows
     for i in range(n):
         pasc = []
-        
         # looping m = columns
         for m in range(i + 1):
             if m  == 0 or m == i:
