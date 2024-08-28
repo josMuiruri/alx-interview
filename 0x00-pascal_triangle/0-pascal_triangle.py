dbb8de090450563242193b0a03649b791dbd3d1d
josMuiruri <josemuiruri51@gmail.com>
2024-08-28 18:02:17 +0300

    document the function

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,10 +4,13 @@ representing the Pascal's triangle'''
 def pascal_triangle(n: int) -> list:
     # assigning an empty list
     triangle = []
+    # less than/equal to 0 return an empty list
     if n <= 0:
         return triangle
+    # looping i = rows
     for i in range(n):
         pasc = []
+        # looping m = columns
         for m in range(i + 1):
             if m  == 0 or m == i:
                 pasc.append(1)
@@ -15,5 +18,3 @@ def pascal_triangle(n: int) -> list:
                 pasc.append(triangle[i - 1][m -1] + triangle[i - 1][m])
         triangle.append(pasc)
     return triangle
-
-# print(pascal_triangle(5))
