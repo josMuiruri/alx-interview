977cf3f17733d21df4ce41b0170f3ead0860237b
josMuiruri <josemuiruri51@gmail.com>
2024-08-31 23:34:09 +0300

    documented the module

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -22,16 +22,20 @@ def island_perimeter(grid: list[list[int]]) -> int:
     for r in range(rows):
         for c in range(cols):
             if grid[r][c] == 1:
-                # check top
+                ''' check top
+                '''
                 if r == 0 or grid[r - 1][c] == 0:
                     perimeter += 1
-                # check bottom
+                ''' check bottom
+                '''
                 if r == rows - 1 or grid[r + 1][c] == 0:
                     perimeter += 1
-                # check left
+                ''' check left
+                '''
                 if c == 0 or grid[r][c - 1] == 0:
                     perimeter += 1
-                # check right
+                ''' check right
+                '''
                 if c == cols - 1 or grid[r][c + 1] == 0:
                     perimeter += 1
     return perimeter
